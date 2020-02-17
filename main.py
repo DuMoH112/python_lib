8ef2090af150f44350faefc9332468deaa97f3d3
DuMoH112 <52354470+DuMoH112@users.noreply.github.com>
2020-02-17 10:57:07 +0300

    Add function searchLineary

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import random
 import time
 
-def searchInArray(array: list, findNumber: int):
+def searchLinary(array: list, findNumber: int):
+    for i in range(len(array)):
+        if array[i] == findNumber:
+            return i
+    else:
+        return "Элемент отсутствует в списке"
+
+def searchBinary(array: list, findNumber: int):
     lengthArray = len(array)
     beginArray = 0
     endArray = lengthArray - 1
     while True:
         center = (endArray - beginArray) // 2 + beginArray
-        # print("\n\n", beginArray, " ", center, " ", endArray)
         if beginArray == center:
             return "Элемент отсутствует в списке"
         if array[center] == findNumber:
             return center
         elif array[center] > findNumber:
             endArray = center
-        # print(array[center], " > ", findNumber)
         else:
             beginArray = center
-        # print(array[center], " < ", findNumber)
 
-
-def sortArray(array: list):
+def sortQuick(array: list):
     for j in range(len(array) - 1):
         for i in range(1, len(array), 1):
             if array[i - 1] > array[i]:
@@ -33,7 +36,7 @@ def main():
     __value = 1500
     array = [random.randint(0, 1500) for i in range(__value)]
     # print(sortArray(array))
-    findNumberIndex = searchInArray(searchInArray(array), findNumber)
+    findNumberIndex = searchLinary(sortArray(array), findNumber)
     print("Элемент под индексом ", findNumberIndex)
 
 tic = time.time()
